cf3349dc1365c7c2c4fa6497c149609cb9ea7892
SilasSpringer <ss132517@ohio.edu>
2022-10-28 13:01:02 -0400

    forced wrapping on pokemon page, should allow for mobile view regardless of browser

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -33,7 +33,7 @@ export default function Pokemon() {
             <button className={`${styles2.Name}`}>
                 {`${Poke.name}`.toUpperCase()}
             </button>
-            <div className="Container" style={{ width: '75vw', flex: '1 1 auto'}}>
+            <div className="Container" style={{ width: '75vw', flex: '1 1 auto', flexWrap: 'wrap'}}>
                 <div style={{width: '15vw', 'min-width': 'var(--Pokemon-min-width)'}}>
                     <StatList stats={Poke.stats}/>
                 </div>
